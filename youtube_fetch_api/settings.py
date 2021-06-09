b23075c9305ce10fd19221f77ee8fd01bf28004f
naman <naman.gupta@squadiq.in>
2021-06-10 02:13:25 +0530

    removed unnessary code

--- youtube_fetch_api/settings.py
+++ youtube_fetch_api/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_cron',
     'django_celery_beat',
     'django_filters',
     'django_crontab',
